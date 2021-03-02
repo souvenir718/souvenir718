3cc158ab909cbe2270941b79f0d0eea0b83d7db5
souvenir718 <mongkey0@naver.com>
2021-03-02 09:29:58 +0900

    test npx

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log(' ');
+console.log(' ');
+console.log('      안녕하세요. 김수빈입니다! 😎');
+console.log(' ');
+console.log('      언제나, 어디서나 다양한 경험을 얻고 즐겁게 일하는');
+console.log('      유연한 Front-End 개발자가 되겠습니다! 🎁');
+console.log(' ');
+console.log('      Github : https://github.com/souvenir718');
+console.log('      Email  : mongkey0@naver.com');
+console.log(' ');
+console.log(' ');
+console.log('      궁금한 점이 있다면 언제든지 연락주세요! 📮');
